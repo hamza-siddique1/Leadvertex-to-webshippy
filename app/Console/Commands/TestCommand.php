06446720ca23375f7e3d60b1f5c90300d1ed3fbd
Hamza Siddique <6793siddique@gmail.com>
2026-02-17 13:01:54 +0500

    test command

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        app('log')->channel('webhooks')->info('Cron worked');
+    }
+}
